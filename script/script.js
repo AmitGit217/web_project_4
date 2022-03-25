5dad0e034fab16ebcede05fe1d211d5f39438097
Practicum AmitBG217 <amit217@yandex.com>
2022-03-25 12:49:17 +0300

    refactor: Applied fixes after QA [all files]

--- script/script.js
+++ script/script.js
@@ -1,10 +1,11 @@
 //Get our elements needed :)
+const popupForm = document.querySelector(".popup__form");
 const likeButton = document.querySelectorAll(".single-element__like-button");
 const editButton = document.querySelector(".profile__edit-button");
 const closeButton = document.querySelector(".popup__close-button");
 const popup = document.querySelector(".popup");
-const popupFullName = document.querySelector(".popup__name");
-const popupDescription = document.querySelector(".popup__description");
+const popupFullName = document.querySelector(".popup__input_name");
+const popupDescription = document.querySelector(".popup__input_description");
 const saveButton = document.querySelector(".popup__submit-button");
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
@@ -17,15 +18,12 @@ editButton.addEventListener("click", popupWindow);
 closeButton.addEventListener("click", popupWindow);
 saveButton.addEventListener("click", popupWindow);
 
-//Change profile name and description using our save button
+// Change profile name and description using our save button
 function changeProfileData(event) {
   event.preventDefault();
   if ((popupFullName.value || popupDescription.value) != "") {
     profileName.textContent = popupFullName.value;
     profileDescription.textContent = popupDescription.value;
-  } else {
-    popupFullName.value = profileName.textContent;
-    popupDescription.value = profileDescription.textContent;
   }
 }
-saveButton.addEventListener("click", changeProfileData);
+popupForm.addEventListener("submit", changeProfileData);
