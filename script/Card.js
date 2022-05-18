f72882ad903fe27c69a9ce763d285c451c3ddfa4
Practicum AmitBG217 <amit217@yandex.com>
2022-05-18 09:53:58 +0300

    refactor: Create Card class without functional abilty

--- script/Card.js
+++ script/Card.js
@@ -1,31 +1,4 @@
-import { openPopup, closePopup } from "./globalFunctions.js";
-
-const initialCards = [
-  {
-    name: "Yosemite Valley",
-    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
-  },
-  {
-    name: "Lake Louise",
-    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
-  },
-  {
-    name: "Bald Mountains",
-    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
-  },
-  {
-    name: "Latemar",
-    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
-  },
-  {
-    name: "Vanoise National Park",
-    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://code.s3.yandex.net/web-code/lago.jpg",
-  },
-];
+import { openPopup, closePopup } from "./utils.js";
 const popupSettings = {
   inputCaption: ".popup__input_addPhoto_caption",
   inputLink: ".popup__input_addPhoto_ImageURL",
@@ -36,7 +9,6 @@ const addCardPopupCaption = document.querySelector(popupSettings.inputCaption);
 const addCardPopupURL = document.querySelector(popupSettings.inputLink);
 const addCardPopupForm = document.querySelector(popupSettings.cardForm);
 const addCardPopup = document.querySelector(popupSettings.addCardPopup);
-
 const cardSettings = {
   cardsSection: ".elements",
   cardImagePopup: ".imagePopup",
@@ -53,7 +25,6 @@ const cardSettings = {
 };
 const cardsSection = document.querySelector(cardSettings.cardsSection);
 const imagePopup = document.querySelector(cardSettings.cardImagePopup);
-
 export class Card {
   constructor(data, template) {
     this._text = data.name;
@@ -66,14 +37,6 @@ export class Card {
       .content.querySelector(cardSettings.cardClass)
       .cloneNode(true);
     return card;
-  }
-  generateCard() {
-    this._card = this._getTemplate();
-    this._card.querySelector(cardSettings.cardImageClass).src = this._link;
-    this._card.querySelector(cardSettings.cardCationClass).textContent =
-      this._text;
-    this._setEventListeners();
-    return this._card;
   }
   _toggleLike() {
     const likeButton = this._card.querySelector(cardSettings.cardLikeButton);
@@ -121,141 +84,20 @@ export class Card {
     this._removeCard();
     this._zoomCard();
   }
+  generateCard() {
+    this._card = this._getTemplate();
+    this._card.querySelector(cardSettings.cardImageClass).src = this._link;
+    this._card.querySelector(cardSettings.cardCationClass).textContent =
+      this._text;
+    this._setEventListeners();
+    return this._card;
+  }
 }
-addCardPopupForm.addEventListener("submit", () => {
-  const data = {
-    name: addCardPopupCaption.value,
-    link: addCardPopupURL.value,
-  };
-  const newCard = new Card(data, cardSettings.cardsTemplate);
-  const cardElement = newCard.generateCard();
-  cardsSection.prepend(cardElement);
-  closePopup(addCardPopup);
-  addCardPopupForm.reset();
-});
-initialCards.forEach((item) => {
-  const card = new Card(item, cardSettings.cardsTemplate);
-  const cardElement = card.generateCard();
-  cardsSection.appendChild(cardElement);
-});
-//////////////////////////////////////////////////////
-//
-// ──── CARDS TEMPLATE AND ZOOM ON VARIABLES ─────
-//
-// const elementsSection = document.querySelector(".elements");
-// const imagePopupPhoto = document.querySelector(".imagePopup__image");
-// const imagePopupCaption = document.querySelector(".imagePopup__caption");
-// const imagePopupCloseButton = document.querySelector(
-//   ".imagePopup__closeButton "
-// );
-// const cardTemplate = document.querySelector("#card-template").content;
-// const addButton = document.querySelector(".profile__add-button");
-// const addCardPopupForm = document.querySelector("#addImagePopup__form");
-// const addCardPopupCloseButton = document.querySelector(
-//   "#add-popup__close-button"
-// );
-// //
-// // ─── CREATE CARD AND THEIR FUNCTIONS ─────
-// //
-// function createCard(card) {
-//   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
-//   const imageElement = cardElement.querySelector(".card__image");
-//   imageElement.src = card.link;
-//   imageElement.alt = card.name;
-//   cardElement.querySelector(".card__caption").textContent = card.name;
-//   //
-//   // ─── LIKE BUTTON OF THIS ELEMENT ─────
-//   //
-//   cardElement
-//     .querySelector(".card__like-button")
-//     .addEventListener("click", (evt) => {
-//       evt.target.classList.toggle("card__like-button_active");
-//     });
-//   // ────────────────────────────────────────────────────────────────────────────────
-//   // ────────────────────────────────────────────────────────────────────────────────
-//   // ────────────────────────────────────────────────────────────────────────────────
-
-//   //
-//   // ─── REMOVE THIS ELEMENT ─────
-//   //
-//   cardElement
-//     .querySelector(".card__removeButton")
-//     .addEventListener("click", () => {
-//       const thisCard = cardElement.closest(".card");
-//       thisCard.remove();
-//     });
-//   // ─────────────────────────────────────────────────────────────────
-//   // ────────────────────────────────────────────────────────────────────────────────
-//   // ────────────────────────────────────────────────────────────────────────────────
-
-//   //
-//   // ───── SHOW IMAGE POPUP OF THIS ELEMENT KNOWN AS ZOOMING ─────
-//   //
-//   cardElement.querySelector(".card__image").addEventListener("click", () => {
-//     imagePopupPhoto.src = card.link;
-//     imagePopupPhoto.alt = card.name;
-//     imagePopupCaption.textContent = card.name;
-//     openPopup(imagePopup);
-//   });
-//   // ────────────────────────────────────────────────────────────────────────────────
-//   // ────────────────────────────────────────────────────────────────────────────────
-//   // ────────────────────────────────────────────────────────────────────────────────
-
-//   //
-//   // ──── CLOSE THE IMAGE POPUP WINDOW AKA UN ZOOMING ─────
-//   //
-//   imagePopupCloseButton.addEventListener("click", () => {
-//     closePopup(imagePopup);
-//   });
-//   // ────────────────────────────────────────────────────────────────────────────────
-//   // ────────────────────────────────────────────────────────────────────────────────
-//   // ────────────────────────────────────────────────────────────────────────────────
-
-//   return cardElement; //All our code inside this return :)
-// }
-
-// //
-// // ──── OPENING AND CLOSING OUR ADD CARD POPUP ─────
-// //
-// addButton.addEventListener("click", () => openPopup(addImagePopup));
-// addCardPopupCloseButton.addEventListener("click", () =>
-//   closePopup(addImagePopup)
-// );
-// // ────────────────────────────────────────────────────────────────────────────────
-// // ────────────────────────────────────────────────────────────────────────────────
-// // ────────────────────────────────────────────────────────────────────────────────
-
-// //
-// // ──────────────────────────────────────────────────────── III ──────────
-// //   :::::: A D D   C A R D : :  :   :    :     :        :          :
-// // ──────────────────────────────────────────────────────────────────
-// //
-// addCardPopupForm.addEventListener("submit", (evt) => {
-//   const addCardPopupCaption = document.querySelector(
-//     ".popup__input_addPhoto_caption"
-//   );
-//   const addCardPopupURL = document.querySelector(
-//     ".popup__input_addPhoto_ImageURL"
-//   );
-//   evt.preventDefault();
-//   elementsSection.prepend(
-//     createCard({
-//       name: addCardPopupCaption.value,
-//       link: addCardPopupURL.value,
-//     })
-//   );
-//   closePopup(addImagePopup);
-
-//   addCardPopupForm.reset();
-// });
-
-// //
-// // ───── CREATE OUR LAYOUT ─────
-// //
-// const card = createCard(initialCards);
-// initialCards.forEach((card) => {
-//   elementsSection.append(createCard(card));
-// });
-// ────────────────────────────────────────────────────────────────────────────────
-// ────────────────────────────────────────────────────────────────────────────────
-// ────────────────────────────────────────────────────────────────────────────────
+export {
+  addCardPopupCaption,
+  addCardPopupURL,
+  addCardPopupForm,
+  addCardPopup,
+  cardsSection,
+  cardSettings,
+};
--- script/script.js
+++ script/script.js
@@ -1,8 +1,60 @@
-import { Card } from "./Card.js";
-import { openPopup, closePopup } from "./globalFunctions.js";
+import {
+  Card,
+  addCardPopupCaption,
+  addCardPopupURL,
+  addCardPopupForm,
+  addCardPopup,
+  cardsSection,
+  cardSettings,
+} from "./Card.js";
+import { openPopup, closePopup } from "./utils.js";
+
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+  },
+];
+addCardPopupForm.addEventListener("submit", () => {
+  const data = {
+    name: addCardPopupCaption.value,
+    link: addCardPopupURL.value,
+  };
+  const newCard = new Card(data, cardSettings.cardsTemplate);
+  const cardElement = newCard.generateCard();
+  cardsSection.prepend(cardElement);
+  closePopup(addCardPopup);
+  addCardPopupForm.reset();
+});
+initialCards.forEach((item) => {
+  const card = new Card(item, cardSettings.cardsTemplate);
+  const cardElement = card.generateCard();
+  cardsSection.appendChild(cardElement);
+});
+
+//////////////////////////////////////////////////////////////////////////////////
 
 const editProfilePopup = document.querySelector("#profilePopup");
-
 const profilePopupForm = document.querySelector("#profilePopup__form");
 const profileEditButton = document.querySelector("#profilePopup__edit-button");
 const profilePopupCloseButton = document.querySelector(
